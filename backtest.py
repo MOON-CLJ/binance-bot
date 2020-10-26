d9a60bfaa4ff5b897489c833e68128a1a261cccf
lijun <lijunli2598@gmail.com>
2020-10-26 23:57:55 +0800

    z

--- backtest.py
+++ backtest.py
@@ -9,13 +9,13 @@ from macd_strategy import Strategy
 class Backtest:
     def __init__(self, starting_amount, start_datetime, end_datetime, strategy):
         # Starting amount
-        self.start = starting_amount
+        self.starting_amount = starting_amount
         # Number of trades
         self.num_trades = 0
         # Number of profitable trades
         self.profitable_trades = 0
         # Running amount
-        self.amount = self.start
+        self.amount = self.starting_amount
         # Start of desired interval
         self.startTime = start_datetime
         # End of desired interval
@@ -32,7 +32,7 @@ class Backtest:
         self.results = self.runBacktest()
 
     def runBacktest(self):
-        amount = self.start
+        amount = self.starting_amount
         klines = self.strategy.getKlines()
         time = self.strategy.getTime()
         point_finder = 0
@@ -55,6 +55,7 @@ class Backtest:
                         active_buy = True
                         buy_price = float(strategy_result[point_finder][4])
                         self.trades.append([strategy_result[point_finder][0], 'BUY', buy_price])
+                        amount = amount * (1 - 0.0075)
                     if strategy_result[point_finder][3] == 'SELL' and active_buy is True:
                         active_buy = False
                         bought_amount = amount / buy_price
@@ -64,6 +65,7 @@ class Backtest:
                             self.profitable_trades += 1
 
                         amount = bought_amount * sell_price
+                        amount = amount * (1 - 0.0075)
                         self.trades.append([strategy_result[point_finder][0], 'SELL', sell_price])
                     point_finder += 1
         self.amount = amount
@@ -79,7 +81,7 @@ class Backtest:
         print("Number of Trades: " + str(self.num_trades))
         profitable = self.profitable_trades / self.num_trades * 100
         print("Percentage of Profitable Trades: " + str(profitable) + "%")
-        percent = self.amount / self.start * 100
+        percent = self.amount / self.starting_amount * 100
         print(str(percent) + "% of starting amount")
         for entry in self.trades:
             print(entry[1] + " at " + str(entry[2]) + " when " + entry[0].isoformat())
@@ -111,10 +113,14 @@ if __name__ == '__main__':
             macd_backtest = Backtest(10000, time[0], time[len(time) - 1], macd_strategy)
             macd_backtests.append(macd_backtest)
             """
-            if macd_backtest.amount - 75 * macd_backtest.num_trades > 11000:
+            if macd_backtest.amount > 11000:
                 macd_strategy.plotIndicator()
             """
-        macd_backtests = sorted(macd_backtests, key=lambda x: x.amount - 75 * x.num_trades)
-        for macd_backtest in macd_backtests[-3:]:
+        macd_backtests = sorted(macd_backtests, key=lambda x: x.amount)
+        cnt = 0
+        for macd_backtest in macd_backtests:
+            if cnt == 3:
+                break
             if macd_backtest.num_trades > 0:
                 macd_backtest.printResults()
+                cnt += 1
